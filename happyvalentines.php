4759cf3032b4203d1a16757cf5022e792095459d
navydecrem <159695538+navydecrem@users.noreply.github.com>
2024-02-11 19:48:14 +0800

    Add files via upload

---
+++ happyvalentines.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="index.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <title>Virtual Valentine's Day Card</title>
+        <title>To  My Most Beautiful Baby </title>
+    </head>
+    <body>
+        <div class="envelope-wrapper">
+            <div id="envelope" class="close">
+                <div class="front flap"></div>
+                <div class="front pocket"></div>
+                <div class="letter">
+                    <p class="words line1">Hello Sophie ko</p>
+                    <p class="words line2">You're burger to my fries!</p>
+                    <p class="words line3">Love,</p>
+                    <p class="words line4">Yvan</p>
+                </div>
+                <div class="hearts">
+                    <div class="heart a1"></div>
+                    <div class="heart a2"></div>
+                    <div class="heart a3"></div>
+                </div>
+            </div>
+        </div>
+        <div class="reset">
+            <button id="open">Open</button>
+            <button id="reset">Reset</button>
+        </div>
+        <h1 class="valentine">Happy Valentine's Day!</h1>
+        <h1 class="valentine">To  My Most Beautiful Baby!</h1>
+        <script src="index.js"></script>
+    </body>
+</html>
